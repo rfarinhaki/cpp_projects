c87d77dc9af3f8e378af460296d0101c5af1a72f
Ricardo Farinhaki <ricardo.farinhaki@polilinux.com.br>
2018-07-10 17:04:37 -0300

    removed test items

--- blockchain/main.cpp
+++ blockchain/main.cpp
@@ -25,22 +25,6 @@ int main()
     c.addBlock(new Info("Gustavo", 33));
     c.addBlock(new Info("Ricardo", 33));
 
-    c.addBlock(new Info("Joao", 74));
-    c.addBlock(new Info("Antonia", 64));
-    c.addBlock(new Info("Monica", 36));
-    c.addBlock(new Info("Gustavo", 33));
-    c.addBlock(new Info("Ricardo", 33));
-    c.addBlock(new Info("Joao", 74));
-    c.addBlock(new Info("Antonia", 64));
-    c.addBlock(new Info("Monica", 36));
-    c.addBlock(new Info("Gustavo", 33));
-    c.addBlock(new Info("Ricardo", 33));
-    c.addBlock(new Info("Joao", 74));
-    c.addBlock(new Info("Antonia", 64));
-    c.addBlock(new Info("Monica", 36));
-    c.addBlock(new Info("Gustavo", 33));
-    c.addBlock(new Info("Ricardo", 33));
-
     for(i=0; i<c.size(); i++)
     {
         Info *info =(Info*) c.getBlock(i)->getInfo();
